14c8b0bcdc73d3e53a32eabeeafece8de12c3ecf
Bruno Tiberio <quentemente@gmail.com>
2022-11-22 07:29:22 -0300

    feat: configuração model users

--- users/models.py
+++ users/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 # Create your models here.
 
+
 class User(AbstractUser):
-    ...
 
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    username = models.CharField(max_length=30, unique=True)
+    first_name = models.CharField(max_length=90)
+    last_name = models.CharField(max_length=120)
 
+    REQUIRED_FIELDS = ["first_name", "last_name"]
