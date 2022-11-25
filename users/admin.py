c7e4664a1368990176d2dd4caaa676a87211b818
Bruno Tiberio <quentemente@gmail.com>
2022-11-25 10:03:17 -0300

    feat: django admin configurado

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+class UserAdmin(UserAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "cpf",
+        "loja",
+    )
+
+    fieldsets = (
+        ("Dados de Login", {"fields": ("username", "password")}),
+        (
+            "Dados Pessoais e Financeiros",
+            {"fields": ("first_name", "last_name", "cpf", "loja")},
+        ),
+    )
+
+
+admin.site.register(User, UserAdmin)
