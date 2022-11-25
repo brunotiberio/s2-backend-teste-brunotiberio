f47b759f99bb9f77b095ac920906f678dcab3dc8
Bruno Tiberio <quentemente@gmail.com>
2022-11-25 13:34:01 -0300

    feat: spectacular schema e doc

--- _project/urls.py
+++ _project/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("core.urls")),
-    path('api/', include('transactions.urls'))
+    path("api/", include("transactions.urls")),
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+    ),
 ]
